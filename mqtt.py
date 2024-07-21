b564a32246e5437136d0629e6ccc95ef5701606a
xinruoyusixian <xinruoyusixian@163.com>
2024-07-22 00:03:43 +0800

    Add files via upload

---
+++ mqtt.py
@@ -0,0 +1,231 @@
+
+
+import usocket as socket
+import ustruct as struct
+from ubinascii import hexlify
+
+
+class mqtt:  
+    def __init__(self,server,port,topic,cb, client_id="esp",keepalive=0):
+        self.client_id = client_id
+        self.sock = None
+        self.server = server
+        self.port = port
+        self.pid = 0
+        self.keepalive = keepalive
+        self.lw_topic = None
+        self.lw_msg = None
+        self.lw_qos = 0
+        self.lw_retain = False
+        self.topic=topic
+        self.cb=cb
+        self.online=None
+        
+
+    def _send_str(self, s):
+        self.sock.write(struct.pack("!H", len(s)))
+        self.sock.write(s)
+
+    def _recv_len(self):
+        n = 0
+        sh = 0
+        while 1:
+            b = self.sock.read(1)[0]
+            n |= (b & 0x7f) << sh
+            if not b & 0x80:
+                return n
+            sh += 7
+
+
+
+    def set_last_will(self,  msg, retain=False, qos=0):
+        assert 0 <= qos <= 2
+        self.lw_topic = self.topic
+        self.lw_msg = msg
+        self.lw_qos = qos
+        self.lw_retain = retain
+    def connect(self, clean_session=True):
+        try:
+          self.sock = socket.socket()
+          addr = socket.getaddrinfo(self.server, self.port)[0][-1]
+          self.sock.connect(addr)
+        except  Exception as e :
+          print("connect:",e)
+          self.online=0
+          self.online=0
+          return
+        premsg = bytearray(b"\x10\0\0\0\0\0")
+        msg = bytearray(b"\x04MQTT\x04\x02\0\0")
+
+        sz = 10 + 2 + len(self.client_id)
+        msg[6] = clean_session << 1
+        if self.keepalive:
+            assert self.keepalive < 65536
+            msg[7] |= self.keepalive >> 8
+            msg[8] |= self.keepalive & 0x00FF
+        if self.lw_topic:
+            sz += 2 + len(self.lw_topic) + 2 + len(self.lw_msg)
+            msg[6] |= 0x4 | (self.lw_qos & 0x1) << 3 | (self.lw_qos & 0x2) << 3
+            msg[6] |= self.lw_retain << 5
+
+        i = 1
+        while sz > 0x7f:
+            premsg[i] = (sz & 0x7f) | 0x80
+            sz >>= 7
+            i += 1
+        premsg[i] = sz
+
+        self.sock.write(premsg, i + 2)
+        self.sock.write(msg)
+        self._send_str(self.client_id)
+        if self.lw_topic:
+            self._send_str(self.lw_topic)
+            self._send_str(self.lw_msg)
+        resp = self.sock.read(4)
+        assert resp[0] == 0x20 and resp[1] == 0x02
+        if resp[3] != 0:
+            print("connect:",resp[3])
+            return
+        self.subscribe()
+        self.online=1
+        return resp[2] & 1
+        
+
+    def disconnect(self):
+        self.sock.write(b"\xe0\0")
+        self.sock.close()
+
+    def ping(self):
+        self.sock.write(b"\xc0\0")
+
+    def publish(self,msg,topic=None, retain=False, qos=0):
+        topic= self.topic if topic==None else topic
+        pkt = bytearray(b"\x30\0\0\0")
+        pkt[0] |= qos << 1 | retain
+        sz = 2 + len(topic) + len(msg)
+
+        if qos > 0:
+            sz += 2
+        assert sz < 2097152
+        i = 1
+        while sz > 0x7f:
+            pkt[i] = (sz & 0x7f) | 0x80
+            sz >>= 7
+            i += 1
+        pkt[i] = sz
+        try:
+          self.sock.write(pkt, i + 1)
+          self._send_str(topic)
+        except Exception as e:
+          print("publish:",e)
+          self.connect(clean_session=False)
+          return
+        if qos > 0:
+            self.pid += 1
+            pid = self.pid
+            struct.pack_into("!H", pkt, 0, pid)
+            self.sock.write(pkt, 2)
+        self.sock.write(msg)
+        if qos == 1:
+            while 1:
+                op = self.wait_msg()
+                if op == 0x40:
+                    sz = self.sock.read(1)
+                    assert sz == b"\x02"
+                    rcv_pid = self.sock.read(2)
+                    rcv_pid = rcv_pid[0] << 8 | rcv_pid[1]
+                    if pid == rcv_pid:
+                        return
+        elif qos == 2:
+            assert 0
+
+    def subscribe(self,  qos=0):
+        pkt = bytearray(b"\x82\0\0\0")
+        self.pid += 1
+        struct.pack_into("!BH", pkt, 1, 2 + 2 + len(self.topic) + 1, self.pid)
+        self.sock.write(pkt)
+        self._send_str(self.topic)
+        self.sock.write(qos.to_bytes(1, "little"))
+        while 1:
+            op = self.wait_msg()
+            if op == 0x90:
+                resp = self.sock.read(4)
+             
+                assert resp[1] == pkt[2] and resp[2] == pkt[3]
+                if resp[3] == 0x80:
+                    print("subscribe:",resp[3])
+                    return
+
+                return
+
+
+    def wait_msg(self):
+        res = self.sock.read(1)
+        try:
+          self.sock.setblocking(True)
+        except Exception as e:
+          print("wait_msg_err:",e)
+          self.connect(clean_session=False)
+          
+        if res is None:
+            return None
+        if res == b"":
+            raise OSError(-1)
+        if res == b"\xd0":  # PINGRESP
+            sz = self.sock.read(1)[0]
+            assert sz == 0
+            return None
+        op = res[0]
+        if op & 0xf0 != 0x30:
+            return op
+        sz = self._recv_len()
+        topic_len = self.sock.read(2)
+        topic_len = (topic_len[0] << 8) | topic_len[1]
+        self.topic = self.sock.read(topic_len)
+        sz -= topic_len + 2
+        if op & 6:
+            pid = self.sock.read(2)
+            pid = pid[0] << 8 | pid[1]
+            sz -= 2
+        msg = self.sock.read(sz)
+        self.cb(self.topic, msg)
+        if op & 6 == 2:
+            pkt = bytearray(b"\x40\x02\0\0")
+            struct.pack_into("!H", pkt, 2, pid)
+            self.sock.write(pkt)
+        elif op & 6 == 4:
+
+            assert 0
+
+    def check_msg(self):
+        try:
+          self.sock.setblocking(False)
+          return self.wait_msg()
+        except Exception as e:
+          print(e)
+          self.connect(clean_session=False)
+
+
+if __name__=="__main__":
+  import time,lib
+    
+  def p_data(topic,msg):
+         print(topic,msg)
+         
+  lib.update_time()
+  mq=mqtt("d023980739a4c6d611f59b9e351b791c","power001",p_data)
+  mq.connect()
+  while 1:
+    #print("%s:%s:%s"%time.localtime()[3:6])
+    
+    mq.check_msg()
+    if time.time()%20==0:
+      print("ping",wifi.ifconfig()[0])
+      mq.publish("1")
+      time.sleep(1)
+    
+
+
+
+
+
---
+++ oled_save_mem.py
@@ -0,0 +1,161 @@
+import framebuf
+class oled:
+    def __init__(self,i2c,height=64,width=128):
+        self.i2c = i2c
+        self.ADDR = 0x3C
+        cmd64= [
+        [0xAE],                    # DISPLAYOFF
+        [0xA4],           #显示恢复
+        [0xD5, 0xF0],            # 设置显示时钟SETDISPLAYCLOCKDIV
+        [0xA8, 0x3F],                  # 设置多重 SETMULTIPLEX
+        [0xD3, 0x00],              # 设置显示偏移SETDISPLAYOFFSET
+        [0 | 0x0],                   # 设定起始行 SETSTARTLINE
+        [0x8D,  0x14],                    # CHARGEPUMP
+        [0x20,  0x00],  #  内存模式水平 MEMORYMODE horizontal
+        [0x21,  0, 127],  # 列地址 COLUMNADDR
+        [0x22,  0, 63],   # 页面地址 PAGEADDR
+        [0xa0 | 0x1],  # SEGREMAP
+        [0xc8],   #  通讯扫描COMSCANDEC
+        [0xDA,  0x12],                    #设置组合 SETCOMPINS
+        [0x81,  0xCF],                   #  SETCONTRAST
+        [0xd9,  0xF1],                  # SETPRECHARGE
+        [0xDB,  0x40],                 # SETVCOMDETECT
+        [0xA6],                 # NORMALDISPLAY
+        [0xd6, 0],  # zoom off
+        ]
+        cmd32=[
+          [0xAE],        	# DISPLAYOFF
+          [0xD5],        	# SETDISPLAYCLOCKDIV
+          [0x80],        	# the suggested ratio 0x80
+          [0xA8],        	# SSD1306_SETMULTIPLEX
+          [0x1F],
+          [0xD3],        	# SETDISPLAYOFFSET
+          [0x00],         	# no offset
+          [0x40 | 0x0],  	# SETSTARTLINE
+          [0x8D],        	# CHARGEPUMP
+          [0x14],         #0x014 enable, 0x010 disable
+
+          [0x20],         #com pin HW config, sequential com pin config (bit 4), disable left/right remap (bit 5),
+          [0x00],         #128x64:0x00     128x32 : 0x02, or 128x32 OLED 0x12  
+
+          [0xa1],         #segment remap a0/a1
+          [0xc8],         #c0: scan dir normal, c8: reverse
+          [0xda],
+
+          [0x02],         #com pin HW config, sequential com pin config (bit 4), disable left/right remap (bit 5)
+          [0x81],
+          [0xcf],         #[2] set contrast control
+          [0xd9],
+          [0xf1],         #[2] pre-charge period 0x022/f1
+          [0xdb],
+          [0x40],         #vcomh deselect level
+
+          [0x2e],         #Disable scroll
+          [0xa4],         #output ram to display
+          [0xa6],         #none inverted normal display mode
+          [0xaf]        #display on
+            ]
+        cmd= cmd32 if(height==32) else cmd64
+        del cmd32
+        del cmd64
+        for c in cmd:
+            self.command(c)
+        self.clear()
+        self.command([0xaf])  # SSD1306_DISPLAYON
+    def set(self,rect):
+      #设置范围 rect[0],rect[1] 从列到几列最大127
+      #         rect[2],rect[3] 从行到几行最大7
+        self.command([0x21, rect[0], rect[1]]) 
+        self.command([0x22, rect[2], rect[3]])
+    def clear(self,rect=[0, 127, 0, 7]):
+        self.set(rect)
+        screen = bytearray(33)
+        screen[0] = 0x40
+        for i in range(0, 32):
+            self.write(screen)
+            
+    def command(self,c):
+        self.i2c.writeto(self.ADDR, b'\x00' + bytearray(c))
+
+        
+    def fill(self,col):
+        self.framebuf.fill(col)
+        
+    def pixel(self,x, y, col):
+        self.framebuf.pixel(x, y, col)
+
+    def scroll(self, dx, dy):
+        self.framebuf.scroll(dx, dy)
+
+    def text(self,string, x, y, col=1):
+        self.framebuf.text(string, x, y, col)        
+    def write(self,b):
+      self.i2c.writeto(self.ADDR, b)
+      # 一个十六进制 控制每一列向下的8个像素
+      #i2c.writeto(ADDR, b'@0\x00\x00\x00\x000')
+
+
+    
+
+    def string(self,s):
+      #在指定定位置写入8*8的文字 会自动向后显示文字
+      w=len(s)*8
+      h=8
+      buffer= bytearray(((h // 8) * w) + 1)
+      buffer[0] = 0x40  
+      fbuf = framebuf.FrameBuffer(memoryview(buffer)[1:], w, h, framebuf.MONO_VLSB)
+      fbuf.text(s, 0, 0, 0xffff)
+      self.write(buffer)        
+
+    def draw(self,arr,x,y):
+        self.set([x,127,y,7])
+        for i in range(0,len(arr)):
+            self.write(arr[i])
+            y+=1 
+            self.set([x,127,y,7])
+    def  fill(self,s,b=1024):
+        p=0  
+        while 1:
+          self.write(s)
+          p+=1
+          if p==b:
+            break
+         
+                     
+       
+           
+if __name__ == "__main__":
+  from machine import Pin, I2C
+  i2c=I2C(scl=Pin(5), sda=Pin(4))
+  font=[
+#气球16x16
+[b"@\x00\x70\xFC\x06\xFA\xFF\xFF\x03\x03\xFF\xFF\xFE\x04\xFC\x70\x00",b"@\x00\x00\x01\x03\x06\x0F\xEF\xFC\xFC\xEF\x0F\x06\x03\x01\x00\x00",  ],
+#8
+[b"@\x00\x1C\x7E\x7F\xE3\xC1\x81\x81\xC3\x7F\x7E\x1C",b"@\x00\x3C\x7E\x7F\xC3\x80\x81\x81\xC3\xFF\x7E\x3C",  ],
+#温
+[b"@\x00\x80\x80\x04\x0C\x18\x18\x80\x00\xF8\xF8\xC8\xC8\xC8\xC8\xC8\xC8\xC8\xFC\xFC\x00\x00\x00\x00",
+b"@\x00\x00\x01\x07\x87\xF8\x7F\x07\xE0\xEF\x2F\x25\xE5\xE5\x25\xE5\xE5\x25\x2F\xEF\xE0\x20\x00\x00",
+b"@\x00\x01\x01\x3F\x3F\x3F\x20\x20\x3F\x3F\x20\x20\x3F\x3F\x20\x3F\x3F\x20\x20\x3F\x3F\x30\x30\x20",],
+#太阳
+[
+  b"@\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xFF\xFF\xFF\xFF\xFF\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00",
+  b"@\x00\x00\x00\x00\x00\x00\x00\x00\x0C\x0E\x1E\x7F\xFE\xF8\xF8\xF8\x70\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x81\x80\x80\x80\x80\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xF0\xF8\xF8\xFC\xFE\x7F\x1E\x0C\x00\x00\x00\x00\x00\x00\x00\x00",
+  b"@\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x01\x01\x00\x80\xC0\xE0\xF0\xF8\xFC\x7E\x3E\x3F\x1F\x1F\x0F\x0F\x0F\x07\x07\x07\x0F\x0F\x0F\x1F\x1F\x3F\x3E\x7E\xFC\xF8\xE0\xE0\xC0\x00\x00\x01\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00",
+  b"@\xC0\xC0\xC0\xC0\xC0\xC0\xC0\xC0\xC0\x00\x00\x00\x00\x00\x00\x00\xFE\xFF\xFF\xFF\x0F\x03\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x03\x1F\xFF\xFF\xFF\xFE\x00\x00\x00\x00\x00\x00\x00\xC0\xC0\xC0\xC0\xC0\xC0\xC0\xC0",
+  b"@\x03\x07\x07\x07\x07\x07\x07\x07\x03\x00\x00\x00\x00\x00\x00\x00\xFF\xFF\xFF\xFF\xF0\xC0\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xC0\xF8\xFF\xFF\xFF\x7F\x00\x00\x00\x00\x00\x00\x00\x03\x07\x07\x07\x07\x07\x07\x07",
+  b"@\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x03\x0F\x0F\x1F\x3F\x7E\x7C\xF8\xF8\xF8\xF0\xF0\xF0\xE0\xE0\xE0\xF0\xF0\xF0\xF8\xF8\xF8\x7C\x7E\x3F\x1F\x0F\x07\x03\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00",
+  b"@\x00\x00\x00\x00\x00\x00\x00\x00\x20\x30\x78\xFE\x3F\x3F\x3F\x0F\x0E\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x01\x01\x01\x01\x81\x01\x01\x01\x01\x01\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x0F\x1F\x3F\x3F\x7F\xFC\x78\x30\x00\x00\x00\x00\x00\x00\x00\x00",
+  b"@\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xFF\xFF\xFF\xFF\xFF\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00",
+  ]
+]
+
+  #默认是高度为64的oled  如果是32的d=oled(i2c,32)
+  d=oled(i2c)
+  #清屏 可带参数清除指定区域 [0,127,0,8]  (0,127) 宽度,(0,8)是高度  1个高度是8像素 
+  d.clear()
+  d.draw(font[3],0,0)
+  d.draw(font[2],65,0)
+  d.draw(font[0],65,3)
+  d.draw(font[1],90,3)
+  d.clear([65,127,5,7]) #清除此区域 在此区域输出
+  d.string("12345-=%as")#使用自带faramebuffer 输出应英文
